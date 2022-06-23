ca138a44c0700f4871e8770d20567f708917d06c
kingofings <tschann.jonas@gmail.com>
2022-06-23 17:24:20 +0200

    Add project files.

---
+++ Number to byte converter/NumericToByte.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumToByte
+{
+    internal class NumericToByte
+    {
+        public string ConvertNumericToByte(string input)
+        {
+            string returnVal = "Invalid entry";
+            float floatValue;
+            int intValue;
+            if (float.TryParse(input, out floatValue))
+            {
+                returnVal = BitConverter.ToString(BitConverter.GetBytes(floatValue));
+            }
+            else if (int.TryParse(input, out intValue))
+            {
+                returnVal = BitConverter.ToString(BitConverter.GetBytes(intValue));
+            }
+            return returnVal;
+        }
+
+        public string FormatToSourcemodSyntax(string input)
+        {
+            string returnVal = "\\x" + input.Replace("-", "\\x");
+            return returnVal;
+        }
+    }
+}
---
+++ Number to byte converter/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using TextCopy;
+namespace NumToByte
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please enter a numeric data type to convert to bytes:");
+            string? buffer = Console.ReadLine();
+
+            NumericToByte bytes = new NumericToByte();
+
+            if (buffer is not null)
+            {
+                buffer = bytes.ConvertNumericToByte(buffer);
+            }
+            
+            if (buffer != "Invalid entry" && buffer is not null)
+            {
+                buffer = bytes.FormatToSourcemodSyntax(buffer);
+                Console.WriteLine();
+                Console.WriteLine("Bytes: " + buffer);
+                Clipboard clipboard = new Clipboard();
+                clipboard.SetText(buffer);
+                Console.WriteLine();
+                Console.WriteLine("Copied to clipboard.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid entry!");
+            }
+            Console.WriteLine();
+            Main();
+        }
+    }
+}
